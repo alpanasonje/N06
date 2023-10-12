e479fa55c919d8150d68143c3e62d23bb0c36c54
Hp <Hp@172.16.13.112>
2023-10-12 12:35:37 +0530

    commit

---
+++ N06/src/com/tnsif/daynineteen/JdbcDemo.java
@@ -0,0 +1,16 @@
+package com.tnsif.daynineteen;
+
+public class JdbcDemo {
+
+	public static void main(String[] args) {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			System.out.println("Driver loaded successfully...");
+		} catch (ClassNotFoundException e) {
+			System.out.println(e.getMessage());
+
+		}
+
+	}
+
+}
